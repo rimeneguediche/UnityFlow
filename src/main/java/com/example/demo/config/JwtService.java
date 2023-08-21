a114618700ab11a250432f1a4f15dafad02ff5ed
rimeneguediche <rimene.guediche@esprit.tn>
2023-08-21 19:35:19 +0100

    some code for auth

--- src/main/java/com/example/demo/config/JwtService.java
+++ src/main/java/com/example/demo/config/JwtService.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Service;
 import java.security.Key;
 import java.security.Signature;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 
@@ -27,12 +28,32 @@ public class JwtService {
         return claimsResolver.apply(claims);
     }
 
+    public String generateToken(UserDetails userDetails){
+        return generateToken(new HashMap<>(), userDetails);
+    }
+
+
     public String generateToken(
     Map<String, Object> extraClaims,
     UserDetails userDetails
             ){
         return  Jwts.builder().setClaims(extraClaims).setSubject(userDetails.getUsername()).setIssuedAt(new Date(System.currentTimeMillis())).setExpiration(new Date(System.currentTimeMillis()+ 1000 * 60 * 24 )).signWith(getSignInKey(), SignatureAlgorithm.ES256).compact();
     }
+
+    public boolean isTokenValid(String token , UserDetails userDetails){
+        final String username = extractUsername(token);
+        return(username.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
     private Claims extractAmmClaims(String token){
         return Jwts.parserBuilder().setSigningKey(getSignInKey()).build().parseClaimsJws(token).getBody();
     }
